ae95bed7cf963ae67114833793f11f9a59c9e6b3
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2019-02-08 20:45:32 -0200

    Módulo 5 - Bancos de Dados - Nosso projeto Multi-banco de dados (fim)

---
+++ 07-multi-db/src/db/strategies/interfaces/interfaceCrud.js
@@ -0,0 +1,25 @@
+class NotImplementedExcepetion extends Error {
+    constructor(){
+        super("Not Implemented Exception")
+    }
+}
+
+class ICrud {
+    create(item){
+        throw new NotImplementedExcepetion()
+    }
+
+    read(query){
+        throw new NotImplementedExcepetion()
+    }
+
+    update(id, menu){
+        throw new NotImplementedExcepetion()
+    }
+
+    delete(id){
+        throw new NotImplementedExcepetion()
+    }
+}
+
+module.exports = ICrud
