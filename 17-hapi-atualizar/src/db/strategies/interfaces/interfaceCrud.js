8cb0674df9f77d5b4da33d0a1fb581be72544774
Gabriel Fonseca <gabrielfonseca@MacBook-Air-de-Gabriel.local>
2019-06-25 22:03:49 -0300

    - Módulo 9 - Atualizando Heróis (PATCH/PUT)

---
+++ 17-hapi-atualizar/src/db/strategies/interfaces/interfaceCrud.js
@@ -0,0 +1,32 @@
+class NotImplementedExcepetion extends Error {
+    constructor(){
+        super("Not Implemented Exception")
+    }
+}
+
+class ICrud {
+    create(item){
+        throw new NotImplementedExcepetion()
+    }
+
+    read(query){
+        throw new NotImplementedExcepetion()
+    }
+
+    update(id, menu){
+        throw new NotImplementedExcepetion()
+    }
+
+    delete(id){
+        throw new NotImplementedExcepetion()
+    }
+
+    isConnected(){
+        throw new NotImplementedExcepetion()
+    }
+    connect(){
+        throw new NotImplementedExcepetion()
+    }
+}
+
+module.exports = ICrud
---
+++ 17-hapi-atualizar/src/routes/heroRoutes.js
@@ -0,0 +1,134 @@
+const BaseRoute = require('./base/baseRoute')
+const Joi = require('joi')
+
+const failAction = (request, headers, erro)=>{
+    throw erro;
+}
+
+class HeroRoutes extends BaseRoute {
+    constructor(db){
+        super()
+        this.db = db;
+    }
+
+    list(){
+        return {
+            path: '/herois',
+            method: 'GET',
+            config: {
+                validate: {
+                    //payload -> body
+                    //headers -> header
+                    //params -> na URL :id
+                    //query -> skip=10&limit=100
+                    failAction,
+                    query: {
+                        skip: Joi.number().integer().default(0),
+                        limit: Joi.number().integer().default(10),
+                        nome: Joi.string().min(3).max(100)
+                    }
+                }
+            },
+            handler: (request, headers) => {
+                try {
+                    const {
+                        skip,
+                        limit, 
+                        nome
+                    } = request.query
+
+                    const query = {
+                        nome: {
+                            $regex: `.*${nome || ""}*.`
+                        }
+                    }
+                    
+                    return this.db.read(nome ? query : {}, skip, limit)
+                }
+                catch(error){
+                    console.log('DEU RUIM!', error);
+                    return "Erro interno no servidor"
+                }
+            }
+        }
+    }
+
+    create(){
+        return {
+            path: '/herois',
+            method: 'POST',
+            config: {
+                validate: {
+                    failAction,
+                    payload: {
+                        nome: Joi.string().required().min(3).max(100),
+                        poder: Joi.string().required().min(2).max(100)
+                    }
+                }
+            },
+            handler: async (request)=>{
+                try {
+                    const {nome, poder} = request.payload
+                    const result = await this.db.create({
+                        nome, poder
+                    })
+                             
+                    return {
+                        message: 'Heroi cadastrado com sucesso!',
+                        _id: result._id
+                    }
+                } catch (error) {
+                    console.log('DEU RUIM', error);
+                    return 'Ínternal Error!'
+                }
+            }
+        }
+    }
+
+    update(){
+        return {
+            path: '/herois/{id}',
+            method: 'PATCH',
+            config: {
+                validate: {
+                    params:{
+                        id: Joi.string().required()
+                    },
+                    payload:{
+                        nome: Joi.string().min(3).max(100),
+                        poder: Joi.string().min(2).max(100)
+                    }
+                }
+            },
+            handler: async (request)=>{
+                try {
+                    const {
+                        id
+                    } = request.params;
+
+                    const {
+                        payload
+                    } = request
+                    const dadosString = JSON.stringify(payload)
+                    const dados = JSON.parse(dadosString)
+
+                    const result = await this.db.update(id, dados)
+                    
+                    if(result.nModified !== 1) return {
+                        message: 'Não foi possível atualizar!'
+                    }
+                    
+                    return {
+                        message: 'Heroi atualizado com sucesso!'
+                    }
+                } catch (error) {
+                    console.error('DEU RUIM', error);
+                    return 'Erro interno!'
+                }
+            }
+        }
+    }
+}
+
+//localhost:500/herois?skip=0&limit=10&nome=flash
+module.exports = HeroRoutes
